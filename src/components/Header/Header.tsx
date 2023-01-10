6ae0800562cb3d62255beb54f4c6c4a7b744f85c
Hugo Deiró <hugodeiro@gmail.com>
2023-01-10 12:00:05 -0300

    Create desktop header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { getRouteDefinitions } from '../../routes';
 import './Header.scss';
+import logo from '../../assets/img/logo-small-amarela.png';
 
 function Header() {
   const routes = getRouteDefinitions();
 
   return (
-    <nav>
-      {routes.map(({ path, title }, index) => (
-        <NavLink
-          to={path}
-          key={`${path}-${index}`}
-          className={({ isActive }) => (isActive ? 'active' : '')}
-        >
-          {title}
+    <header className="header">
+      <div className="header-logo">
+        <NavLink to="/">
+          <img
+            src={logo}
+            className="header-logo-image"
+            alt="Index Gestão Contábil"
+          />
         </NavLink>
-      ))}
-    </nav>
+      </div>
+
+      <nav className="header-navigation">
+        {routes.map(({ path, title }, index) => (
+          <NavLink
+            to={path}
+            key={`${path}-${index}`}
+            className={({ isActive }) =>
+              'header-navigation-link ' + (isActive ? 'active' : '')
+            }
+          >
+            {title}
+          </NavLink>
+        ))}
+      </nav>
+    </header>
   );
 }
 
