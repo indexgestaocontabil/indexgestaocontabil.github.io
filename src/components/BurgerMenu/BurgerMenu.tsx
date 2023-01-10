2318e43cc0ae437606c3ef4ea84f73136ac40d45
Hugo Deiró <hugodeiro@gmail.com>
2023-01-10 20:20:49 -0300

    Add mobile menu

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useEffect } from 'react';
 import './BurgerMenu.scss';
-import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdMenu, MdClose } from 'react-icons/md';
 import Ripples from 'react-ripples'
+import useToggle from '../../hooks/use-toggle';
+import { useLocation } from 'react-router-dom';
 
 type PropsType = {
   children: React.ReactNode;
 }
 
+type ValidIconType = typeof MdMenu;
+
 function BurgerMenu({ children }: PropsType) {
-  console.log(children);
-  return (
-    <>
-      <Ripples color='rgba(255, 255, 255, .3)' className='menu-trigger-ripple'>
-        <button type="button" className="menu-trigger">
-          <GiHamburgerMenu size={'24px'} className='menu-trigger-icon'/>
+  const [isMenuOpen, toggleMenu] = useToggle(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    toggleMenu(false);
+  }, [location, toggleMenu]);
+
+  function renderButton(Icon: ValidIconType, callback: () => void) {
+    return (
+      <Ripples color='rgba(255, 255, 255, .3)' className='menu-mobile-trigger-ripple menu-mobile-opener'>
+        <button type="button" 
+          className="menu-mobile-trigger" 
+          onClick={callback}>
+          <Icon size={'24px'} className='menu-mobile-trigger-icon'/>
         </button>
       </Ripples>
-    </>
+    )
+  }
+
+  return (
+    <div className='menu-mobile'>
+      {renderButton(MdMenu, () => toggleMenu(true))}  
+
+      {isMenuOpen && (
+        <div className='menu-mobile-opened-container'>
+          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+          <div className="menu-mobile-opened-overlay" onClick={() => toggleMenu(false)} />
+          <div className='menu-mobile-opened-content'>
+            {renderButton(MdClose, () => toggleMenu(false))}  
+
+            <div className="menu-mobile-opened-content-links">
+              {children}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
---
+++ src/hooks/use-toggle.ts
@@ -0,0 +1,15 @@
+import { useCallback, useState } from "react"
+
+type UseToggleOutput = [boolean, (forcedState?: boolean) => void];
+
+const useToggle = (defaultValue = false): UseToggleOutput => {
+  const [state, setState] = useState(defaultValue)
+
+  const toggleState = useCallback((forcedState?: boolean) => {
+    setState(currentValue => typeof forcedState === "boolean" ? forcedState : !currentValue)
+  }, []);
+
+  return [state, toggleState];
+};
+
+export default useToggle;
