bd83e7836a3fc996a1921f1cee2f03673b54f5b1
Hugo Deiró <hugodeiro@gmail.com>
2023-01-12 17:22:21 -0300

    Add Email Service

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -2,7 +2,34 @@ import React from 'react';
 import './ContactPage.scss';
 
 function ContactPage() {
-  return <h1>Contact Page</h1>;
+  const onSubmit = () => {
+    fetch('https://formsubmit.co/ajax/hugodeiro@gmail.com', {
+      method: 'POST',
+      body: JSON.stringify({
+        Nome: 'Hugo Deiró',
+        Mensagem: 'Blablablabla',
+        Teste: 'Teste123',
+        Email: 'hugodeiro@gmail.com'
+      }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      console.log('success!');
+    }).catch(() => {
+      console.log('Failure!');
+    });
+  }
+
+  return (
+    // <form action="https://formsubmit.co/indexcontabilidade@outlook.com" method="POST">
+    <div>
+      <button onClick={onSubmit}>
+        Enviar Teste
+      </button>
+    </div>
+  );
 }
 
 export default ContactPage;
