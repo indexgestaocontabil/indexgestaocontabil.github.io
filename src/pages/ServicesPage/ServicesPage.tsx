cd0594e454da7b2bb051faedac0a3abf3083c160
Hugo Deiró <hugodeiro@gmail.com>
2023-01-20 20:47:04 -0300

    Services Page

--- src/pages/ServicesPage/ServicesPage.tsx
+++ src/pages/ServicesPage/ServicesPage.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Title from '../../components/Title/Title';
 import './ServicesPage.scss';
+import services from './services.json';
 
 const ServicesPage = () => (
-  <div className="content-wrapper">
-    <h1>Services Page</h1>
+  <div className="services-page">
+    <div className="content-wrapper header-wrapper">
+      <Title content="Serviços que oferecemos" marginBottom={8} />
+    </div>
+
+    {services.map(({ title, description, activities }, index) => (
+      <section className="services-page-section" key={`service-${index}`}>
+        <Title level={2} content={title} marginBottom={16} subtitled={false} />
+        <p className="services-page-section-description">{description}</p>
+        <div className="tagset">
+          {activities.map((activity, indexActivity) => (
+            <div
+              className="tag"
+              key={`service-${index}-activity-${indexActivity}`}
+            >
+              {activity}
+            </div>
+          ))}
+        </div>
+      </section>
+    ))}
   </div>
 );
 
