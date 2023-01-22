79734287aec1eb2bf20cd78eaf02961dfae04714
Hugo Deiró <hugodeiro@gmail.com>
2023-01-21 23:44:13 -0300

    Create base structure of about page

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -2,14 +2,51 @@ import React from 'react';
 import Hero from '../../components/Hero/Hero';
 import './AboutPage.scss';
 import AboutContent from './content.json';
+import logo from '../../assets/img/logo-big-amarela.png';
+import Title from '../../components/Title/Title';
+import Tag from '../../components/Tag/Tag';
 
 const AboutPage = () => (
-  <>
+  <div className="about">
     <Hero {...AboutContent.hero} />
-    <div className="content-wrapper">
-      <h1>About Page</h1>
-    </div>
-  </>
+    <section className="about-us content-wrapper">
+      <div className="about-us-content">
+        {AboutContent.about.map((content, index) => (
+          <p key={`content-${index}`} className="about-us-content-paragraph">
+            {content}
+          </p>
+        ))}
+      </div>
+
+      <img
+        src={logo}
+        alt="Logotipo da Index Gestão Contábil"
+        className="logo"
+      />
+    </section>
+
+    <section className="about-mission content-wrapper">
+      <Title level={2} content="Nossa Missão" marginBottom={8} />
+      <p className="about-mission-content">{AboutContent.mission}</p>
+    </section>
+
+    <section className="about-vision content-wrapper">
+      <Title level={2} content="Nossa Visão" marginBottom={8} />
+      <p className="about-vision-content">{AboutContent.vision}</p>
+    </section>
+
+    <section className="about-values content-wrapper">
+      <Title level={2} content="Nossos Valores" marginBottom={8} />
+
+      <p className="about-values-text">{AboutContent.values.text}</p>
+
+      <div className="tagset">
+        {AboutContent.values.tags.map(({ label }, index) => (
+          <Tag text={label} key={`tag-${index}`} />
+        ))}
+      </div>
+    </section>
+  </div>
 );
 
 export default AboutPage;
--- src/utils/utilitary-functions.ts
+++ src/utils/utilitary-functions.ts
@@ -6,3 +6,17 @@ export const getInstagramLink = (account: string) =>
 
 export const getLinkedinLink = (account: string) =>
   `https://www.linkedin.com/in/${account}/`;
+
+// Ref: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
+export const generateUUID = () => {
+  let dt = new Date().getTime();
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+    /[xy]/g,
+    function (c) {
+      const r = (dt + Math.random() * 16) % 16 | 0;
+      dt = Math.floor(dt / 16);
+      return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+    }
+  );
+  return uuid;
+};
