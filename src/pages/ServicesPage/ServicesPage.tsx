6437350bae992c6c9cfc55e0a9bb746950966f9c
Hugo Deiró <hugodeiro@gmail.com>
2023-01-21 22:49:25 -0300

    Update content for Services

--- src/pages/ServicesPage/ServicesPage.tsx
+++ src/pages/ServicesPage/ServicesPage.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import Title from '../../components/Title/Title';
 import './ServicesPage.scss';
-import services from './services.json';
+import ServicesContent from './content.json';
 import Hero from '../../components/Hero/Hero';
-import { HERO } from '../../globals';
 import Tag from '../../components/Tag/Tag';
 
 const ServicesPage = () => (
   <div className="services-page">
-    <Hero {...HERO.services} />
+    <Hero {...ServicesContent.hero} />
 
-    {services.map(({ title, description, activities }, index) => (
-      <section className="services-page-section" key={`service-${index}`}>
-        <Title level={2} content={title} marginBottom={16} subtitled={false} />
-        <p className="services-page-section-description">{description}</p>
-        <div className="tagset">
-          {activities.map((activity, indexActivity) => (
-            <Tag
-              key={`service-${index}-activity-${indexActivity}`}
-              text={activity}
-            />
-          ))}
-        </div>
-      </section>
-    ))}
+    {ServicesContent.sections.map(
+      ({ title, description, activities }, index) => (
+        <section className="services-page-section" key={`service-${index}`}>
+          <Title
+            level={2}
+            content={title}
+            marginBottom={16}
+            subtitled={false}
+          />
+          <p className="services-page-section-description">{description}</p>
+          <div className="tagset">
+            {activities.map((activity, indexActivity) => (
+              <Tag
+                key={`service-${index}-activity-${indexActivity}`}
+                text={activity}
+              />
+            ))}
+          </div>
+        </section>
+      )
+    )}
   </div>
 );
 
