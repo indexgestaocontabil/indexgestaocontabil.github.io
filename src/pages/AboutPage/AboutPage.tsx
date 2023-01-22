9be045cff8e1d7114da3acaa4b21189479084c66
Hugo Deiró <hugodeiro@gmail.com>
2023-01-21 22:56:27 -0300

    Update content for About

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Hero from '../../components/Hero/Hero';
 import './AboutPage.scss';
+import AboutContent from './content.json';
 
 const AboutPage = () => (
-  <div className="content-wrapper">
-    <h1>About Page</h1>
-  </div>
+  <>
+    <Hero {...AboutContent.hero} />
+    <div className="content-wrapper">
+      <h1>About Page</h1>
+    </div>
+  </>
 );
 
 export default AboutPage;
