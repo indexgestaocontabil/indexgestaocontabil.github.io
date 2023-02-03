ce5f7ae5daddf3ec5dbb580d42b65dfff3329801
Hugo Deiró <hugodeiro@gmail.com>
2023-02-03 20:54:14 -0300

    Update Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { getRouteDefinitions } from '../../routes';
 import './Header.scss';
-import logo from '../../assets/img/logo-dourada.svg';
+import { ReactComponent as Logo } from '../../assets/img/logo-dourada.svg';
 import useBreakpointObserver from '../../hooks/use-breakpoint-observer';
 import { useCallback } from 'react';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
@@ -9,43 +9,43 @@ import BurgerMenu from '../BurgerMenu/BurgerMenu';
 const Header = () => {
   const { isDesktop } = useBreakpointObserver();
 
-  const getNavigationItems = useCallback((withDivider: boolean) => {
-    const routes = getRouteDefinitions();
+  const getNavigationItems = useCallback(
+    (withDivider: boolean) => {
+      const routes = getRouteDefinitions();
+      const clazz = `is-${isDesktop ? 'desktop' : 'mobile'}`;
 
-    return routes.map(({ path, title }, index) => (
-      <div
-        className="header-navigation-item"
-        key={`header-navigation-item-${index}`}
-      >
-        <NavLink
-          to={path}
-          key={`${path}-${index}`}
-          className={({ isActive }) =>
-            'header-navigation-link ' + (isActive ? 'active' : '')
-          }
+      return routes.map(({ path, title }, index) => (
+        <div
+          className={`header-navigation-item ${clazz}`}
+          key={`header-navigation-item-${index}`}
         >
-          {title}
-        </NavLink>
-        {withDivider && index < routes.length - 1 && (
-          <div className="divider">|</div>
-        )}
-      </div>
-    ));
-  }, []);
+          <NavLink
+            to={path}
+            key={`${path}-${index}`}
+            className={({ isActive }) =>
+              'header-navigation-link ' + (isActive ? 'active' : '')
+            }
+          >
+            {title}
+          </NavLink>
+          {withDivider && index < routes.length - 1 && (
+            <div className="divider">|</div>
+          )}
+        </div>
+      ));
+    },
+    [isDesktop]
+  );
 
   return (
     <header className="header">
       <div className="header-logo">
         <NavLink to="/">
-          <img
-            src={logo}
-            className="header-logo-image"
-            alt="Index Gestão Contábil"
-          />
+          <Logo className="header-logo-image" />
         </NavLink>
       </div>
 
-      <nav className="header-navigation">
+      <nav className={`header-navigation`}>
         {isDesktop ? (
           getNavigationItems(true)
         ) : (
