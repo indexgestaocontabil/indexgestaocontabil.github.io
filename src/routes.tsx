2a8e36b106be3b521f9fb27f51740221bb30c8e0
Index | Gestão Contábil <122317263+indexgestaocontabil@users.noreply.github.com>
2026-09-10 13:26:55 -0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import FloatingWhatsApp from './components/FloatingWhatsApp/FloatingWhatsApp';
+import FloatingWhatsApp from './components/FloatingWhatsApp';
 import { PARAMS } from './globals';
 import AboutPage from './pages/AboutPage/AboutPage';
 import ContactPage from './pages/ContactPage/ContactPage';
