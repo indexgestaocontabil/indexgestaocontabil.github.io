41c777c65c74fe01337e65c9bbedf6719a7f69aa
Index | Gestão Contábil <122317263+indexgestaocontabil@users.noreply.github.com>
2026-09-10 13:42:53 -0300

    Add files via upload

--- src/utils/utilitary-functions.ts
+++ src/utils/utilitary-functions.ts
@@ -1,5 +1,9 @@
-export const getWhatsAppLink = (phoneNumber: string) =>
-  `https://wa.me/${phoneNumber}`;
+export const getWhatsAppLink = (phoneNumber: string) => {
+  const digits = phoneNumber.replace(/\D/g, '');
+  const withCountryCode = digits.startsWith('55') ? digits : `55${digits}`;
+
+  return `https://wa.me/${withCountryCode}`;
+};
 
 export const getGoogleMapsLink = (address: string) =>
   `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
